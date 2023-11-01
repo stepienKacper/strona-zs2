4ec720dc979a95297a5cadfd4ce41592dcf463cb
Kacper Stępień <101900810+stepienKacper@users.noreply.github.com>
2023-11-01 20:20:08 +0100

    55

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleClassButton = document.getElementById("toggleButton");
+
+  toggleClassButton.addEventListener("click", function () {
+    alert("test");
+  });
+});
